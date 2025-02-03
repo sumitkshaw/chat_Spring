59906b59c98bfee4769dbdcd80ff010fd3f95209
sumitkshaw <shawsumit6286@gmail.com>
2025-02-04 02:47:50 +0530

    Initial commit

---
+++ src/main/java/com/sumitshaww/chatSpring/model/ChatMessage.java
@@ -0,0 +1,14 @@
+package com.sumitshaww.chatSpring.model;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ChatMessage
+{
+    private Long id;
+    private String sender;
+    private String content;
+}
---
+++ src/test/java/com/sumitshaww/chatSpring/ChatSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumitshaww.chatSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
